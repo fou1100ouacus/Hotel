45014150762061ae36375d75d4816bd5c99de5c9
Aya Mohamed😎 <152428721+fou1100ouacus@users.noreply.github.com>
2025-05-20 16:39:43 +0300

    Add files via upload

---
+++ Bussiness/clsCustomer.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess;
+
+namespace BussinessLayer
+{
+    public class clsCustomer
+    {
+        public enum enMode { AddNew = 0, Update = 1 };
+        public enMode Mode = enMode.AddNew;
+
+        public int    CustomerID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public string PassportID { get; set; }
+
+        public clsCustomer()
+        {
+            this.CustomerID = -1;
+            this.FirstName = string.Empty;
+            this.LastName = string.Empty;
+            this.Email = string.Empty;
+            this.Phone = string.Empty;
+            this.Address = string.Empty;
+            this.PassportID = string.Empty;
+
+            Mode = enMode.AddNew;
+        }
+
+        private clsCustomer(int CustomerID, string FirstName, string LastName, string Email,
+            string Phone, string Address, string PassportID)
+        {
+            this.CustomerID = CustomerID;
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+            this.Email = Email;
+            this.Phone = Phone;
+            this.Address = Address;
+            this.PassportID = PassportID;
+
+            Mode = enMode.Update;
+        }
+
+        private int _AddNewCustomer()
+        {
+            int id = ClsCustomerData.AddNewCustomer(this.FirstName, this.LastName, this.Email,  this.Phone, this.Address, this.PassportID);
+           
+            
+            return id ;
+        }
+
+        private bool _UpdateCustomer()
+        {
+            return ClsCustomerData.UpdateCustomer(this.CustomerID, this.FirstName, this.LastName,
+                this.Email, this.Phone, this.Address, this.PassportID);
+        }
+
+        public bool Save()
+        {
+            switch (Mode)
+            {
+                case enMode.AddNew:
+                    if (_AddNewCustomer()!=-1)
+                    {
+                        Mode = enMode.Update;
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+
+                case enMode.Update:
+                    return _UpdateCustomer();
+            }
+
+            return false;
+        }
+
+        public static clsCustomer Find(int CustomerID)
+        {
+            string FirstName = "", LastName = "", Email = "", Phone = "", Address = "", PassportID = "";
+
+            bool IsFound = ClsCustomerData.GetCustomerInfoByID(CustomerID, ref FirstName, ref LastName,
+                ref Email, ref Phone, ref Address, ref PassportID);
+
+            if (IsFound)
+            {
+                return new clsCustomer(CustomerID, FirstName, LastName, Email, Phone, Address, PassportID);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public static bool DeleteCustomer(int CustomerID)
+        {
+            if (ClsCustomerData.DeleteCustomer(CustomerID))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public static bool DoesCustomerExist(int CustomerID)
+        {
+            return ClsCustomerData.DoesCustomerExist(CustomerID);
+        }
+
+        public static DataTable GetAllCustomers()
+        {
+            return ClsCustomerData.GetAllCustomers();
+        }
+
+        public static int GetCustomersCount()
+        {
+            return ClsCustomerData.GetCustomersCount();
+        }
+    }
+}
---
+++ Bussiness/clsPayment.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess;
+
+namespace BussinessLayer
+{
+    public class clsPayment
+    {
+        public enum enMode { AddNew = 0, Update = 1 };
+        public enMode Mode = enMode.AddNew;
+
+        public int PaymentID { get; set; }
+        public int BookingID { get; set; }
+        public DateTime PaymentDate { get; set; }
+        public decimal PaidAmount { get; set; }
+        public int CreatedByUserID { get; set; }
+
+        public clsPayment()
+        {
+            this.PaymentID = -1;
+            this.BookingID = -1;
+            this.PaymentDate = DateTime.Now;
+            this.PaidAmount = 0.0m;
+            this.CreatedByUserID = -1;
+
+            Mode = enMode.AddNew;
+        }
+
+        private clsPayment(int PaymentID, int BookingID, DateTime PaymentDate, decimal PaidAmount, int CreatedByUserID)
+        {
+            this.PaymentID = PaymentID;
+            this.BookingID = BookingID;
+            this.PaymentDate = PaymentDate;
+            this.PaidAmount = PaidAmount;
+            this.CreatedByUserID = CreatedByUserID;
+
+            Mode = enMode.Update;
+        }
+
+        private bool _AddNewPayment()
+        {
+            bool result = clsPaymentData.AddNewPayment(this.BookingID, this.PaymentDate, this.PaidAmount, this.CreatedByUserID);
+
+            return result;
+        }
+
+        private bool _UpdatePayment()
+        {
+            return clsPaymentData.UpdatePayment(this.PaymentID, this.BookingID, this.PaymentDate, this.PaidAmount, this.CreatedByUserID);
+        }
+
+        public bool Save()
+        {
+            switch (Mode)
+            {
+                case enMode.AddNew:
+                    if (_AddNewPayment())
+                    {
+                        Mode = enMode.Update;
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+
+                case enMode.Update:
+                    return _UpdatePayment();
+            }
+
+            return false;
+        }
+
+        public static clsPayment Find(int PaymentID)
+        {
+            int BookingID = -1, CreatedByUserID = -1;
+            DateTime PaymentDate = DateTime.Now;
+            decimal PaidAmount = 0.0m;
+
+            bool IsFound = clsPaymentData.GetPaymentInfoByID(PaymentID, ref BookingID, ref PaymentDate, ref PaidAmount, ref CreatedByUserID);
+
+            if (IsFound)
+            {
+                return new clsPayment(PaymentID, BookingID, PaymentDate, PaidAmount, CreatedByUserID);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public static bool DeletePayment(int PaymentID)
+        {
+            return clsPaymentData.DeletePayment(PaymentID);
+        }
+
+        public static bool DoesPaymentExist(int PaymentID)
+        {
+            return clsPaymentData.DoesPaymentExist(PaymentID);
+        }
+
+        public static DataTable GetAllPayments()
+        {
+            return clsPaymentData.GetAllPayments();
+        }
+
+        public static int GetPaymentsCount()
+        {
+            return clsPaymentData.GetPaymentsCount();
+        }
+    }
+}
