63e521883429cea6ab1d8035f7091419cfa9f3dc
Aya Mohamed😎 <152428721+fou1100ouacus@users.noreply.github.com>
2025-05-20 16:39:10 +0300

    Add files via upload

---
+++ Bussiness/clsRoomTypes.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess;
+
+namespace BussinessLayer
+{
+
+
+        public class clsRoomTypes
+        {
+            public enum enMode { AddNew = 0, Update = 1 };
+            public enMode Mode = enMode.AddNew;
+
+            public int RoomTypeID { get; set; }
+            public string RoomTypeTitle { get; set; }
+            public byte RoomTypeCapacity { get; set; }
+            public decimal RoomTypePricePerNight { get; set; }
+            public string RoomTypeDescription { get; set; }
+
+            public clsRoomTypes()
+            {
+                this.RoomTypeID = -1;
+                this.RoomTypeTitle = string.Empty;
+                this.RoomTypeCapacity = 0;
+                this.RoomTypePricePerNight = 0.0m;
+                this.RoomTypeDescription = string.Empty;
+
+                Mode = enMode.AddNew;
+            }
+
+            private clsRoomTypes(int RoomTypeID, string RoomTypeTitle, byte RoomTypeCapacity,
+                decimal RoomTypePricePerNight, string RoomTypeDescription)
+            {
+                this.RoomTypeID = RoomTypeID;
+                this.RoomTypeTitle = RoomTypeTitle;
+                this.RoomTypeCapacity = RoomTypeCapacity;
+                this.RoomTypePricePerNight = RoomTypePricePerNight;
+                this.RoomTypeDescription = RoomTypeDescription;
+
+                Mode = enMode.Update;
+            }
+
+            private bool _AddNewRoomType()
+            {
+                return RoomTypesData.AddNewRoomType(this.RoomTypeTitle, this.RoomTypeCapacity,
+                    this.RoomTypePricePerNight, this.RoomTypeDescription);
+            }
+
+            private bool _UpdateRoomType()
+            {
+                return DataAccess.RoomTypesData.UpdateRoomType((int)this.RoomTypeID, this.RoomTypeTitle,
+                    this.RoomTypeCapacity, this.RoomTypePricePerNight, this.RoomTypeDescription);
+            }
+
+            public bool Save()
+            {
+                switch (Mode)
+                {
+                    case enMode.AddNew:
+                        if (_AddNewRoomType())
+                        {
+                            Mode = enMode.Update;
+                            return true;
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    case enMode.Update:
+                        return _UpdateRoomType();
+                }
+                return false;
+            }
+
+            public static clsRoomTypes Find(int RoomTypeID)
+            {
+                string RoomTypeTitle = string.Empty;
+                byte RoomTypeCapacity = 0;
+                decimal RoomTypePricePerNight = 0.0m;
+                string RoomTypeDescription = string.Empty;
+
+                bool IsFound = DataAccess.RoomTypesData.GetRoomTypeInfoByID(RoomTypeID, ref RoomTypeTitle,
+                    ref RoomTypeCapacity, ref RoomTypePricePerNight, ref RoomTypeDescription);
+
+                if (IsFound)
+                {
+                    return new clsRoomTypes(RoomTypeID, RoomTypeTitle, RoomTypeCapacity,
+                        RoomTypePricePerNight, RoomTypeDescription);
+                }
+                else
+                {
+                    return null;
+                }
+            }
+
+            public static bool DoesRoomTypeExist(int RoomTypeID)
+            {
+                return DataAccess.RoomTypesData.DoesRoomTypeExist(RoomTypeID);
+            }
+
+            public static bool DoesRoomTypeExist(string RoomTypeTitle)
+            {
+                return DataAccess.RoomTypesData.DoesRoomTypeExist(RoomTypeTitle);
+            }
+
+            public static bool DeleteRoomType(int RoomTypeID)
+            {
+                if (DataAccess.RoomTypesData.DeleteRoomType(RoomTypeID))
+                {
+                    return true;
+                }
+                return false;
+            }
+
+            public static System.Data.DataTable GetAllRoomTypes()
+            {
+                return DataAccess.RoomTypesData.GetAllRoomTypes();
+            }
+
+            public static int GetRoomTypesCount()
+            {
+                return DataAccess.RoomTypesData.GetRoomTypesCount();
+            }
+        public static DataTable GetRoomTypes()
+        {
+            return DataAccess.RoomTypesData.GetRoomTypes();
+        }
+    }
+
+    
+
+}
+
+
+
+
+
---
+++ Bussiness/clsUser.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess;
+
+namespace BussinessLayer
+{
+    public class clsUser
+    {
+        
+            public enum enMode { AddNew = 0, Update = 1 };
+            public enMode Mode = enMode.AddNew;
+
+           
+            public int     UserID { get; set; }
+            public string Username { get; set; }
+            public string Password { get; set; }
+      
+            public clsUser()
+            {
+                this.UserID = -1;
+                this.Username = string.Empty;
+                this.Password = string.Empty;
+              
+
+                Mode = enMode.AddNew;
+            }
+
+            private clsUser(int UserID, string Username, string Password )
+            {
+              
+                this.UserID = UserID;
+                this.Username = Username;
+                this.Password = Password;
+              
+                Mode = enMode.Update;
+            }
+
+            private bool _AddNewUser()
+            {
+                return clsUserData.AddNewUser(  this.Username, this.Password  );
+
+               
+            }
+
+            private bool _UpdateUser()
+            {
+                return DataAccess.clsUserData.UpdateUser((int)this.UserID, this.Username, this.Password);
+            }
+
+            public bool Save()
+            {
+
+                
+                switch (Mode)
+                {
+                    case enMode.AddNew:
+                        if (_AddNewUser())
+                        {
+                            Mode = enMode.Update;
+                            return true;
+                        }
+                        else
+                        {
+                            return false;
+                        }
+
+                    case enMode.Update:
+                        return _UpdateUser();
+                }
+
+                return false;
+            }
+
+        
+
+
+
+            public static clsUser Find(string Username, string Password)
+            {
+                int UserID = -1;
+              
+
+                bool IsFound = DataAccess.clsUserData.GetUserInfoByUsernameAndPassword(ref UserID,Username, Password);
+
+                if (IsFound)
+                {
+
+
+                    return new clsUser(  UserID, Username, Password    );
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        public static bool DoesUserExist(string Username, string Password)
+        {
+            return clsUserData.DoesUserExist(Username, Password);
+        }
+        public static bool DeleteUser(int UserID)
+            {
+               
+
+                if (DataAccess.clsUserData.DeleteUser(UserID))
+                {
+                return true;
+                }
+
+                return false;
+            }
+
+            public static bool DoesUserExist(int UserID)
+            {
+                return DataAccess.clsUserData.DoesUserExist(UserID);
+            }
+
+            public static bool DoesUserExist(string Username)
+            {
+                return DataAccess.clsUserData.DoesUserExist(Username);
+            }
+
+    
+
+            public static DataTable GetAllUsers()
+            {
+                return DataAccess.clsUserData.GetAllUsers();
+            }
+
+            public static int GetUsersCount()
+            {
+                return DataAccess.clsUserData.GetUsersCount();
+            }
+
+            public bool ChangePassword(string NewPassword)
+            {
+                return ChangePassword(this.UserID, NewPassword);
+            }
+
+            public static bool ChangePassword(int? UserID, string NewPassword)
+            {
+                return DataAccess.clsUserData.ChangePassword(UserID, NewPassword);
+            }
+        
+    
+    }
+    
+
+}
