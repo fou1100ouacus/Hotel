ecc9470be704753aa8fd237b53158ad90d6df9b3
Aya Mohamed😎 <152428721+fou1100ouacus@users.noreply.github.com>
2025-05-20 16:39:27 +0300

    Add files via upload

---
+++ Bussiness/clsReservation.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess;
+
+namespace BusinessLayer
+{
+    public enum enReservationStatus : byte
+    {
+        Pending = 0,
+        Confirmed = 1,
+        Canceled = 2,
+        Completed = 3,
+        NoShow = 4
+    }
+
+    public class clsReservation
+    {
+        public enum enMode { AddNew = 0, Update = 1 };
+        public enMode Mode = enMode.AddNew;
+
+        public int ReservationID { get; set; }
+        public int RoomID { get; set; }
+        public byte NumberOfPeople { get; set; }
+        public enReservationStatus Status { get; set; }
+        public DateTime LastStatusDate { get; set; }
+        public int CustomerID { get; set; }
+        public DateTime CreatedDate { get; set; }
+
+        public clsReservation()
+        {
+            this.ReservationID = -1;
+            this.RoomID = -1;
+            this.NumberOfPeople = 0;
+            this.Status = enReservationStatus.Pending;
+            this.LastStatusDate = DateTime.MinValue;
+            this.CustomerID = -1;
+            this.CreatedDate = DateTime.MinValue;
+
+            Mode = enMode.AddNew;
+        }
+
+        private clsReservation(int ReservationID, int RoomID, byte NumberOfPeople, enReservationStatus Status,
+            DateTime LastStatusDate, int CustomerID, DateTime CreatedDate)
+        {
+            this.ReservationID = ReservationID;
+            this.RoomID = RoomID;
+            this.NumberOfPeople = NumberOfPeople;
+            this.Status = Status;
+            this.LastStatusDate = LastStatusDate;
+            this.CustomerID = CustomerID;
+            this.CreatedDate = CreatedDate;
+
+            Mode = enMode.Update;
+        }
+
+        private bool _AddNewReservation()
+        {
+            int newReservationID = -1;
+            bool result = ReservationData.AddNewReservation(this.ReservationID, this.RoomID, this.NumberOfPeople,
+                (byte)this.Status, this.LastStatusDate, this.CustomerID, this.CreatedDate);
+
+            if (result)
+            {
+                this.ReservationID = newReservationID; // Note: Requires ReservationData.AddNewReservation to return ID
+                return true;
+            }
+            return false;
+        }
+
+        private bool _UpdateReservation()
+        {
+            return ReservationData.UpdateReservation(this.ReservationID, this.RoomID, this.NumberOfPeople,
+                (byte)this.Status, this.LastStatusDate, this.CustomerID, this.CreatedDate);
+        }
+
+        public bool Save()
+        {
+            switch (Mode)
+            {
+                case enMode.AddNew:
+                    if (_AddNewReservation())
+                    {
+                        Mode = enMode.Update;
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+
+                case enMode.Update:
+                    return _UpdateReservation();
+            }
+
+            return false;
+        }
+
+        public static clsReservation Find(int ReservationID)
+        {
+            int RoomID = -1;
+            byte NumberOfPeople = 0;
+            byte Status = 0;
+            DateTime LastStatusDate = DateTime.MinValue;
+            int CustomerID = -1;
+            DateTime CreatedDate = DateTime.MinValue;
+
+            bool IsFound = ReservationData.GetReservationInfoByID(ReservationID, ref RoomID, ref NumberOfPeople,
+                ref Status, ref LastStatusDate, ref CustomerID, ref CreatedDate);
+
+            if (IsFound)
+            {
+                return new clsReservation(ReservationID, RoomID, NumberOfPeople, (enReservationStatus)Status,
+                    LastStatusDate, CustomerID, CreatedDate);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public static bool DeleteReservation(int ReservationID)
+        {
+            return ReservationData.DeleteReservation(ReservationID);
+        }
+
+        public static bool DoesReservationExist(int ReservationID)
+        {
+            return ReservationData.DoesReservationExist(ReservationID);
+        }
+
+        public static DataTable GetAllReservations()
+        {
+            return ReservationData.GetAllReservations();
+        }
+
+        public static int GetReservationsCount()
+        {
+            return ReservationData.GetReservationsCount();
+        }
+    }
+}
---
+++ Bussiness/clsRoom.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccess;
+
+namespace BussinessLayer
+{
+    public class clsRooms
+    {
+        public enum enMode { AddNew = 0, Update = 1 };
+        public enMode Mode = enMode.AddNew;
+
+        public int RoomID { get; set; }
+        public int RoomTypeID { get; set; }
+        public string RoomNumber { get; set; }
+        public byte RoomFloor { get; set; }
+        public decimal RoomSize { get; set; }
+        public byte AvailabilityStatus { get; set; }
+        public bool IsSmokingAllowed { get; set; }
+        public bool IsPetFriendly { get; set; }
+        public string AdditionalNotes { get; set; }
+
+        public clsRooms()
+        {
+            this.RoomID = -1;
+            this.RoomTypeID = 0;
+            this.RoomNumber = string.Empty;
+            this.RoomFloor = 0;
+            this.RoomSize = 0.0m;
+            this.AvailabilityStatus = 0;
+            this.IsSmokingAllowed = false;
+            this.IsPetFriendly = false;
+            this.AdditionalNotes = string.Empty;
+
+            Mode = enMode.AddNew;
+        }
+
+        private clsRooms(int RoomID, int RoomTypeID, string RoomNumber, byte RoomFloor,
+            decimal RoomSize, byte AvailabilityStatus, bool IsSmokingAllowed, bool IsPetFriendly,
+            string AdditionalNotes)
+        {
+            this.RoomID = RoomID;
+            this.RoomTypeID = RoomTypeID;
+            this.RoomNumber = RoomNumber;
+            this.RoomFloor = RoomFloor;
+            this.RoomSize = RoomSize;
+            this.AvailabilityStatus = AvailabilityStatus;
+            this.IsSmokingAllowed = IsSmokingAllowed;
+            this.IsPetFriendly = IsPetFriendly;
+            this.AdditionalNotes = AdditionalNotes;
+
+            Mode = enMode.Update;
+        }
+
+        private bool _AddNewRoom()
+        {
+            return RoomData.AddNewRoom(this.RoomTypeID, this.RoomNumber, this.RoomFloor,
+                this.RoomSize, this.AvailabilityStatus, this.IsSmokingAllowed, this.IsPetFriendly,
+                this.AdditionalNotes);
+        }
+
+        private bool _UpdateRoom()
+        {
+            return DataAccess.RoomData.UpdateRoom((int)this.RoomID, this.RoomTypeID, this.RoomNumber,
+                this.RoomFloor, this.RoomSize, this.AvailabilityStatus, this.IsSmokingAllowed,
+                this.IsPetFriendly, this.AdditionalNotes);
+        }
+
+        public bool Save()
+        {
+            switch (Mode)
+            {
+                case enMode.AddNew:
+                    if (_AddNewRoom())
+                    {
+                        Mode = enMode.Update;
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                case enMode.Update:
+                    return _UpdateRoom();
+            }
+            return false;
+        }
+
+        public static clsRooms Find(int RoomID)
+        {
+            int RoomTypeID = 0;
+            string RoomNumber = string.Empty;
+            byte RoomFloor = 0;
+            decimal RoomSize = 0.0m;
+            byte AvailabilityStatus = 0;
+            bool IsSmokingAllowed = false;
+            bool IsPetFriendly = false;
+            string AdditionalNotes = string.Empty;
+
+            bool IsFound = DataAccess.RoomData.GetRoomInfoByID(RoomID, ref RoomTypeID, ref RoomNumber,
+                ref RoomFloor, ref RoomSize, ref AvailabilityStatus, ref IsSmokingAllowed,
+                ref IsPetFriendly, ref AdditionalNotes);
+
+            if (IsFound)
+            {
+                return new clsRooms(RoomID, RoomTypeID, RoomNumber, RoomFloor,
+                    RoomSize, AvailabilityStatus, IsSmokingAllowed, IsPetFriendly,
+                    AdditionalNotes);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public static bool DoesRoomExist(int RoomID)
+        {
+            return DataAccess.RoomData.DoesRoomExist(RoomID);
+        }
+
+        public static bool DoesRoomExist(string RoomNumber)
+        {
+            return DataAccess.RoomData.DoesRoomExist(RoomNumber);
+        }
+
+        public static bool DeleteRoom(int RoomID)
+        {
+            if (DataAccess.RoomData.DeleteRoom(RoomID))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static DataTable GetAllRooms()
+        {
+            return DataAccess.RoomData.GetAllRooms();
+        }
+
+        public static int GetRoomsCount()
+        {
+            return DataAccess.RoomData.GetRoomsCount();
+        }
+    }
+}
